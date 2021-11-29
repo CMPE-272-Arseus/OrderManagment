34aa19d7e3b4c8ffcb2eb6e0818649f6c0b368aa
Fuyu Zhang <iamnickchang@hotmail.com>
2021-11-28 23:54:59 -0600

    v1

---
+++ getAllOrder.py
@@ -0,0 +1,42 @@
+import boto3
+import json
+from boto3.dynamodb.conditions import Key, Attr
+import decimal
+
+def get_allOrder(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    if 'userId' not in event['queryStringParameters']:
+        return returnResponse(502, "Bad request, please correct the Query Strings to userId")
+
+    userId = event['queryStringParameters']['userId']
+    table = dynamodb.Table('PhotoPrinter-orderDB')
+    response = table.scan(
+        FilterExpression=Attr('userId').contains(userId)
+    )
+    print(response)
+    if response['Items'] == None or len(response['Items']) == 0:
+        return returnResponse(203, " The user doet have order")
+    return returnResponse(200, json.dumps(response['Items'], indent=4, cls=DecimalEncoder))
+    
+    
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+def returnResponse(statusCode, message):
+    return {
+        'statusCode': statusCode,
+        'body': message,
+        'headers': {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            'Access-Control-Allow-Credentials': True
+        }
+    }
